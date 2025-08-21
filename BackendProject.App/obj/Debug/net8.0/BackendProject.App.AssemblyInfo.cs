7d9da6d50ca0c8d8cb827813df0a1eb80d978642
Shefa <zeyna@example.com>
2025-08-21 10:02:34 +0400

    commit1

--- BackendProject.App/obj/Debug/net8.0/BackendProject.App.AssemblyInfo.cs
+++ BackendProject.App/obj/Debug/net8.0/BackendProject.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendProject.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0cf0462a99a08caec3d3375a98ffeee2968e3f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendProject.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendProject.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
